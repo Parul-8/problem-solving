9f93704822664cd1e6833b0fe7bc5154c210628c
Parul Yadav <108748100+Parul-8@users.noreply.github.com>
2022-11-08 18:28:01 +0530

    Time: 69 ms (15.58%), Space: 14 MB (11.70%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,9 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        l1 = list('abcdefghijklmnopqrstuvwxyz')
-        l1.sort()
-        s1 = ''.join(list(set(l1)))
-        l2 = list(sentence)
-        l2.sort()
-        s2 = ''.join(list(set(l2)))
-        if s1 == s2:
+        s = list(sentence)
+        s.sort()
+        s = set(s)
+        if (len(s) == 26):
             return True
         else:
             return False
